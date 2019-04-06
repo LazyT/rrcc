31e8cd6597f0626ab362ade4beccd9709a76374b
LazyT <lazyt@mailbox.org>
2019-04-06 10:47:09 +0200

    bump version to 0.8.3

--- mainwindow.h
+++ mainwindow.h
@@ -2,8 +2,8 @@
 #define MAINWINDOW_H
 
 #define APPNAME "RoboRock Control Center"
-#define APPVERS "0.8.2"
-#define APPDATE "31.10.2018"
+#define APPVERS "0.8.3"
+#define APPDATE "06.04.2019"
 
 #include <QApplication>
 #include <QMainWindow>
